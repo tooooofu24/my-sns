a90f3b4b18669b48a4e008a9eb6cc663f8659844
tooooofu24 <toya24xxx@i.softbank.jp>
2023-10-09 20:32:11 +0900

    曜日表示

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,10 +1,11 @@
 import { Badge } from "@mantine/core";
 import { format } from "date-fns";
+import { ja } from "date-fns/locale";
 
 export const DateComponent = ({ time }: { time: Date }) => {
   return (
     <Badge fz="xs" color="gray" mx="auto" my={5} radius="xs">
-      {format(time, "M月d日")}
+      {format(time, "M月d日(E)", { locale: ja })}
     </Badge>
   );
 };
