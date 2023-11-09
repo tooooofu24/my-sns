b1b0f03bd97ea136f407effc4cc34d5ff45249bd
tooooofu24 <toya24xxx@i.softbank.jp>
2023-11-10 07:21:03 +0900

    add: 修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,43 @@
-export function Footer() {
-  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   const comment = e.currentTarget.comment.value;
-  //   if (!comment) return;
-  //   mutate(comment);
-  //   e.currentTarget.comment.value = "";
-  // };
+import { FaPaperPlane } from "react-icons/fa6";
+import { useMutation } from "react-query";
 
-  // const { mutate, isLoading } = useMutation(postComment, {
-  //   onSuccess: () => onOpen(),
-  //   onError: () => {
-  //     alert(
-  //       "コメントの投稿に失敗しました🥺\n陶也まで連絡してもらえると助かります！"
-  //     );
-  //   },
-  // });
+import { postComment } from "@/utils/postComment";
+
+export function Footer() {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const comment = e.currentTarget.comment.value;
+    if (!comment) return;
+    mutate(comment);
+    e.currentTarget.comment.value = "";
+  };
+
+  const { mutate, isLoading } = useMutation(postComment, {
+    onSuccess: () =>
+      alert(
+        "コメントを投稿しました！\nページに反映されるまで5分程度かかります。"
+      ),
+    onError: () =>
+      alert(
+        "コメントの投稿に失敗しました🥺\n陶也まで連絡してもらえると助かります！"
+      ),
+  });
 
   return (
     <div className="navbar bottom-0 fixed bg-base-100 z-10">
       <div className="container max-w-md mx-auto">
-        <a className="btn btn-ghost normal-case text-xl">千葉陶也</a>
+        <form className="flex w-full gap-1 items-center" onSubmit={onSubmit}>
+          <textarea
+            className="textarea textarea-bordered flex-1"
+            placeholder="匿名でコメントできます！"
+            rows={1}
+            name="comment"
+          ></textarea>
+          <button type="submit" className="btn btn-ghost btn-circle text-lg">
+            {isLoading && <span className="loading loading-spinner"></span>}
+            {!isLoading && <FaPaperPlane />}
+          </button>
+        </form>
       </div>
     </div>
   );
