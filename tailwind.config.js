489c85daa37bb90de03c3a6ac12780ba2f6abc66
tooooofu24 <toya24xxx@i.softbank.jp>
2023-11-12 01:40:45 +0900

    add: 修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
 
