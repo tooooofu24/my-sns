5cf7e99452cc38fe1074cdc68adac2dea2e455a0
tooooofu24 <toya24xxx@i.softbank.jp>
2023-11-12 01:07:41 +0900

    add: 修正

--- src/components/Tweet/ImageModal.tsx
+++ src/components/Tweet/ImageModal.tsx
@@ -12,20 +12,23 @@ type ImageModalProps = {
 export const ImageModal = memo(({ images, onClose }: ImageModalProps) => {
   return (
     <>
-      <Modal.Body>
-        <Carousel className="w-full rounded-box flex items-center">
+      <Modal.Body className="flex items-center justify-center">
+        <Carousel
+          className="rounded-box w-full align-middle items-center"
+          snap="center"
+        >
           {images.map((image) => (
             <Carousel.Item
               key={image.url}
               alt={image.url}
-              className="rounded-box w-full max-h-[60vh] h-auto"
+              className="rounded-box max-h-[65vh] h-auto w-auto max-w-full align-middle"
             >
               <Image
                 src={image.url}
                 alt="投稿した画像"
                 width={400}
                 height={400}
-                className="rounded-box w-auto h-auto max-w-full max-h-[60vh] object-contain mx-auto"
+                className="rounded-box object-cover max-h-[65vh]"
               />
             </Carousel.Item>
           ))}
