5ad598498d191adb60bb8dcc47986e760800721d
tooooofu24 <toya24xxx@i.softbank.jp>
2023-11-12 01:23:25 +0900

    add: 空のページを追加

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,66 @@
+import { format } from "date-fns";
+import { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useRef } from "react";
+
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Tweet } from "@/components/Tweet/Tweet";
+import { TweetWrapper } from "@/components/Tweet/TweetWrapper";
+import { TweetType } from "@/types/TweetType";
+
+type MainPageProps = {
+  tweets: TweetType[];
+};
+
+export const MainPage: NextPage<MainPageProps> = ({ tweets }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    ref?.current?.scrollIntoView();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>千葉陶也 BLOG</title>
+        <meta
+          name="description"
+          content="LINEのチャット風なブログ。誰でも匿名で返信ができます。"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=5.0"
+        />
+
+        <meta property="og:url" content="https://toya-blog.vercel.app" />
+        <meta property="og:title" content="千葉陶也 BLOG" />
+        <meta
+          property="og:description"
+          content="LINEのチャット風なブログ。誰でも匿名で返信ができます。"
+        />
+        <meta
+          property="og:image"
+          content="https://toya-blog.vercel.app/avatar.jpg"
+        />
+        <meta property="og:site_name" content="千葉陶也 BLOG" />
+        <meta property="og:locale" content="ja_JP" />
+      </Head>
+      <>
+        <Header />
+        <TweetWrapper>
+          <div />
+          {tweets.map((tweet, i) => {
+            const current = new Date(tweet.createdAt);
+            const prev = new Date(tweets[i - 1]?.createdAt ?? null);
+            const showDate =
+              format(current, "yyyy-MM-dd") != format(prev, "yyyy-MM-dd");
+            return <Tweet key={tweet.id} tweet={tweet} showDate={showDate} />;
+          })}
+          <div ref={ref} />
+        </TweetWrapper>
+        <Footer />
+      </>
+    </>
+  );
+};
--- src/pages/empty.tsx
+++ src/pages/empty.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from "next";
 
-import { client } from "@/client";
 import { MainPage } from "@/components/MainPage";
 import { TweetType } from "@/types/TweetType";
-import { fakeTweets } from "@/utils/fakeTweets";
 
 export type HomeProps = {
   tweets: TweetType[];
@@ -15,22 +13,9 @@ const Home: NextPage<HomeProps> = ({ tweets }) => {
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
-  if (process.env.ENV === "local") {
-    return {
-      props: {
-        tweets: fakeTweets,
-      },
-    };
-  }
-
-  const data = await client.get({
-    endpoint: "tweets",
-    queries: { orders: "createdAt", limit: 100 },
-  });
-
   return {
     props: {
-      tweets: data.contents,
+      tweets: [],
     },
   };
 };
