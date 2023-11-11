a11505723d75ecd8a0e395261af6b8f74cd66c3b
tooooofu24 <toya24xxx@i.softbank.jp>
2023-11-12 03:49:23 +0900

    add: 修正

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -28,7 +28,7 @@ export const MainPage: NextPage<MainPageProps> = ({ tweets }) => {
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=5.0"
+          content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=5.0 user-scalable=no"
         />
 
         <meta property="og:url" content="https://toya-blog.vercel.app" />
